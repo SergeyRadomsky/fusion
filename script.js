14f492d2466a17e24e2cdc5057f0e68598ca0a00
SergeyRadomsky <46030768+SergeyRadomsky@users.noreply.github.com>
2022-12-19 13:13:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict'
+// 
+// alert("Я javascript");
+// let admin, name;
+// name = "Джон";
+// admin = name;
+// alert(admin);
+
+let result = prompt("Текст приветствие", 1);
+alert(result);
+console.log(result)
+
+
+
